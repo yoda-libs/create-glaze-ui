58f5d799ead1462f1bd2d07c9199098d07366ef1
Juliano Teixeira <jzucchetto@gmail.com>
2021-12-27 23:05:58 -0800

    Fixed repo url

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import { Glaze } from 'glaze-ui';
 
 const glaze = new Glaze({debug: true});
-glaze.registerApp('@test/test', '/test-test.js', '/test', document.getElementById('test'));
+
+// run command below to create a react micro-frontend
+//
+// npx create-glaze-ui-react-app navbar
+//
+// uncomment line below to register react micro-frontend
+glaze.registerApp({
+    type: 'react',
+    name: 'navbar', 
+    url: 'http://localhost:8081/navbar.js',
+    route: '/', 
+    container: document.getElementById('navbar')
+});
+
 glaze.bootstrap().then(() => {
+    //dispatch test message to all micro-frontend apps
     glaze.dispatch({ test: "message" });
 });
