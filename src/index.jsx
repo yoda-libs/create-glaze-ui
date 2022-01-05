6559b7707e76f715b3501f8e3427b399a44a0883
Juliano Teixeira <jzucchetto@gmail.com>
2022-01-05 12:01:37 -0800

    cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -9,30 +9,7 @@ import { createRoutes, route, createLayout } from 'glaze-ui';
 //
 // uncomment line below to register react micro-frontend
 const apps = createApps([
-    app('login', 'http://localhost:8081/login.js'),
-    app('navbar', 'http://localhost:8083/navbar.js'),
-    
-    app('content', 'http://localhost:9000/content.js'),
-    app('sidebar', 'http://localhost:9000/sidebar.js'),
-    
-    // -------------------------------------------------------
-    // example on how to manually import an external glaze app
-    // -------------------------------------------------------
-    // app('navbar', () => System.import('http://localhost:8083/navbar.js'))
-
-    // ---------------------------------------------
-    // example on how to manually render a glaze app
-    // ---------------------------------------------
-    // app('navbar', {
-    //     mount: (container) => {
-    //         const div = <div>rendered navbar</div>;
-    //         container.appendChild(div);
-    //         return div;
-    //     },
-    //     unmount: (container, app) => {
-    //         container.removeChild(app)
-    //     }
-    // })
+    app('navbar', 'http://localhost:8081/navbar.js'),
 ]);
 
 const layout = createLayout(
@@ -40,20 +17,13 @@ const layout = createLayout(
         <div className="row">
             <div id="navbar" />
         </div>
-        <div className='row'>
-            <div id="content" className='col-sm-8' />
-            <div id="sidebar" className='col-sm-4' />
-        </div>
     </div>
 , {
     navbar: apps['navbar'],
-    content: apps['content'],
-    sidebar: apps['sidebar']
 });
 
 const router = createRoutes([
     route('/', layout),
-    route('/login', apps['login']),
 ])
 
 
@@ -67,9 +37,6 @@ bootstrap({
     },
     options: { debug: true }
 }).then(async glaze => {
-    // example on how to mount a glaze app without a router
-    // await apps['navbar'].mount(document.getElementById('root'));
-
     // example on how to send a message to a glaze app
     glaze.dispatch({test: "message"});
 }).catch(console.error);
